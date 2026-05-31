a3598fc01a8c35b383404481d9fa4d50ae8cee65
jseabaugh <jeff@seabaugh.net>
2026-05-31 17:41:52 -0600

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+{
+  "name": "excel-smart-format",
+  "version": "1.0.0",
+  "description": "Non-volatile conditional formatting Excel add-in",
+  "scripts": {
+    "dev": "webpack serve --mode development",
+    "build": "webpack --mode production",
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@microsoft/office-js": "^1.1.0"
+  },
+  "devDependencies": {
+    "copy-webpack-plugin": "^11.0.0",
+    "css-loader": "^6.8.1",
+    "html-webpack-plugin": "^5.5.3",
+    "style-loader": "^3.3.3",
+    "webpack": "^5.88.2",
+    "webpack-cli": "^5.1.4",
+    "webpack-dev-server": "^4.15.1"
+  }
+}
